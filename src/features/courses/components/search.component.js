33dc21ec18a67ac8226369d969ef0bfebf4637bd
Ana-teston <anapaulateston@gmail.com>
2022-07-24 12:11:21 +0200

    UI features and search & safe area components

---
+++ src/features/courses/components/search.component.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Searchbar } from "react-native-paper";
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
