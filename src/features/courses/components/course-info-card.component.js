921a76487f771cb6de0b378bd9292ed94a4778ac
Ana-teston <anapaulateston@gmail.com>
2022-07-24 20:17:42 +0200

    Theme and custom fonts

--- src/features/courses/components/course-info-card.component.js
+++ src/features/courses/components/course-info-card.component.js
@@ -4,23 +4,26 @@ import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 
 const CourseCard = styled(Card)`
-  background-color: white;
+  background-color: ${(props) => props.theme.colors.ui.quaternary};
 `;
 
 const CourseCardCover = styled(Card.Cover)`
-  padding: 20px;
-  background-color: white;
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.ui.quaternary};
 `;
 
 const Title = styled(Text)`
-  padding: 16px;
-  color: red;
+  font-family: ${(props) => props.theme.fonts.body};
+  padding: ${(props) => props.theme.space[3]};
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 export const CourseInfoCard = ({ course = {} }) => {
   const {
     name = "Some course",
-    info,
+    info = [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl condimentum id venenatis a. Proin sagittis nisl rhoncus mattis rhoncus urna neque viverra. Neque vitae tempus quam pellentesque nec nam. Mattis rhoncus urna neque viverra justo nec ultrices. Turpis egestas sed tempus urna et pharetra pharetra massa massa. Sit amet cursus sit amet dictum sit amet justo donec. Augue ut lectus arcu bibendum at varius. Fusce id velit ut tortor pretium viverra. Rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui. Hac habitasse platea dictumst quisque sagittis purus. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra. Vivamus at augue eget arcu dictum varius duis. Dis parturient montes nascetur ridiculus mus. Nulla porttitor massa id neque aliquam vestibulum morbi blandit cursus. Bibendum neque egestas congue quisque egestas. At urna condimentum mattis pellentesque id nibh tortor id.",
+    ],
     photos = [
       "https://images.unsplash.com/photo-1593642634315-48f5414c3ad9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODF8fHRlY2hub2xvZ3l8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
     ],
--- src/features/courses/screens/courses.screen.js
+++ src/features/courses/screens/courses.screen.js
@@ -1,30 +1,31 @@
 import React from "react";
-import { SafeAreaView, View, StatusBar, StyleSheet } from "react-native";
+import { SafeAreaView, StatusBar } from "react-native";
 import { Searchbar } from "react-native-paper";
+import styled from "styled-components/native";
 import { CourseInfoCard } from "../components/course-info-card.component";
 
-export const CoursesScreen = () => (
-  <SafeAreaView style={styles.container}>
-    <View style={styles.search}>
-      <Searchbar />
-    </View>
-    <View style={styles.list}>
-      <CourseInfoCard />
-    </View>
-  </SafeAreaView>
-);
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight} && "margin-top: ${StatusBar.currentHeight}px";
+`;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "#18A47A",
-  },
-});
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const CourseListContainer = styled.View`
+  flex: 1;
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.brand.primary};
+`;
+
+export const CoursesScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+    <CourseListContainer>
+      <CourseInfoCard />
+    </CourseListContainer>
+  </SafeArea>
+);
